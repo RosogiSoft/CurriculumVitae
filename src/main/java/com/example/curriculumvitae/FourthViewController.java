9e25e7c1087d1b9c28e8b38c6e114f0c32388232
rosogi <rosogisoft@gmial.com>
2022-10-03 18:30:14 +0300

    Added third_view.fxml, and it's design and project of it's controller

--- src/main/java/com/example/curriculumvitae/FourthViewController.java
+++
@@ -1,5 +0,0 @@
-package com.example.curriculumvitae;
-
-public class FourthViewController {
-
-}
---
+++ src/main/java/com/example/curriculumvitae/ThirdViewController.java
@@ -0,0 +1,48 @@
+package com.example.curriculumvitae;
+
+import javafx.scene.control.CheckBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThirdViewController {
+
+    public CheckBox checkBox1;
+    public CheckBox checkBox2;
+    public CheckBox checkBox3;
+    public CheckBox checkBox4;
+    public CheckBox checkBox5;
+    public CheckBox checkBox6;
+    public CheckBox checkBox7;
+    public CheckBox checkBox8;
+    public CheckBox checkBox9;
+    public CheckBox checkBox10;
+    public CheckBox checkBox11;
+    public CheckBox checkBox12;
+    public CheckBox checkBox13;
+    public CheckBox checkBox14;
+    public CheckBox checkBox15;
+
+    public void initialize(){
+        ArrayList<CheckBox> checkBoxes = new ArrayList<>();
+        checkBoxes.add(checkBox1);
+        checkBoxes.add(checkBox2);
+        checkBoxes.add(checkBox3);
+        checkBoxes.add(checkBox4);
+        checkBoxes.add(checkBox5);
+        checkBoxes.add(checkBox6);
+        checkBoxes.add(checkBox7);
+        checkBoxes.add(checkBox8);
+        checkBoxes.add(checkBox9);
+        checkBoxes.add(checkBox10);
+        checkBoxes.add(checkBox11);
+        checkBoxes.add(checkBox12);
+        checkBoxes.add(checkBox13);
+        checkBoxes.add(checkBox14);
+        checkBoxes.add(checkBox15);
+    }
+
+
+
+
+}
